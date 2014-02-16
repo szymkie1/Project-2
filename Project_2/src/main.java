7daf68b17f6bb10bb81278d9026f1ffc9ea087f0
Ryan Szymkiewicz <szymkie1@uic.edu>
2014-02-15 23:04:34 -0600

    Start
    Learning to do this…

---
+++ Project_2/src/main.java
@@ -0,0 +1,8 @@
+
+
+class main{
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+        System.out.println("THis is cool!");
+    }
+}
